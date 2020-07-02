3640043776eab0de16c2466c0dee7014bae3a0aa
18518472310sun <sunhan21>
2020-07-02 16:55:33 +0800

    as

--- manage/controller/Comment.php
+++ manage/controller/Comment.php
@@ -6,9 +6,13 @@ namespace app\manage\controller;
 
 use app\common\controller\ManageBase;
 use think\facade\Log;
-
+use think\Request;
 class Comment extends ManageBase
 {
+   protected $beforeActionList = [
+        'beforeEdit'  =>  ['only'=>'edit,add'],
+    ];
+   
     public function initialize()
     {
         parent::initialize();
@@ -25,7 +29,7 @@ class Comment extends ManageBase
 //        $lists = model('user_comment')->alias('c')->join($join)->field($field)->where($where)->order('c.create_time desc')->paginate(20);
         $join  = [['user u','u.id = f.broker_id'],['second_house s','s.id = f.house_id']];
         $field = 'f.*,u.nick_name,s.title';
-        $lists = model('fydp')->alias('f')->join($join)->field($field)->where($where)->paginate(20);
+        $lists = model('fydp')->alias('f')->join($join)->field($field)->where($where)->paginate(20,false,['query'=>request()->param()]);//7.2sun#分页失效，分页时传递参数
 
 
         $this->_exclude = 'edit';
@@ -38,7 +42,7 @@ class Comment extends ManageBase
         $map = [];
 //        $status = input('get.status');
         $admin_id = input('get.admin_id');
-        $map['f.broker_id'] = $admin_id;
+        $map['f.user_id'] = $admin_id;//7.2sun#修改搜索专员为搜索评论人
         $data = [
             'broker_id'  => $admin_id,
         ];
@@ -54,5 +58,42 @@ class Comment extends ManageBase
         \app\common\model\Comment::event('after_delete',function($obj){
         });
         parent::delete();
+    }
+
+    /**
+     * ajax修改单个字段值
+    
+    public function ajaxedit()
+    {
+        //AJAX修改数据
+        $mod = model($this->_name);
+        $pk  = $mod->getPk();
+        $id  = input($pk);
+        $field = input('param.field');
+        $val   = input('param.val');
+        $mod->where([$pk => $id])->setField($field, $val);
+        return $this->ajaxReturn(1);
+
+    } */
+    /**
+     * ajax修改单个字段值
+     */
+    public function beforeEdit()
+    {
+        if($this->request->isPost()){
+            $id = input('post.id');
+            $house_name = input('post.house_name');
+             $arr = model('fydp')->where(array("id"=>$id))->update(['house_name' =>$house_name]);
+             $this->success('保存成功','Comment/index');
+             
+
+        }else{
+            $id = input('param.id/d');
+            $join  = [['user u','u.id = f.broker_id'],['second_house s','s.id = f.house_id']];
+            $field = 'f.*,u.nick_name,s.title';
+            $arr = model('fydp')->alias('f')->join($join)->field($field)->where(array("f.id"=>$id))->find();//7.2sun#分页失效，分页时传递参数
+            $this->assign('arr',$arr);
+            return $this->fetch();
+        }
     }
 }
